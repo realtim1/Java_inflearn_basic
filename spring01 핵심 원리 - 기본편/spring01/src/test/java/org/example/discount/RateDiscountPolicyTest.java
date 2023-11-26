17aeb85d8a761f15f7ca906b16a93ad04195bf72
realtim1 <99743414+realtim1@users.noreply.github.com>
2023-11-26 22:56:36 +0900

    인텔리제이로 커밋해보기 1

---
+++ spring01 핵심 원리 - 기본편/spring01/src/test/java/org/example/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,38 @@
+package org.example.discount;
+
+import org.example.member.Grade;
+import org.example.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy test = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP는 10% 할인이 적용되어야 한다.")
+    void vip_o(){
+        //given
+        Member member = new Member(1L,"VIP", Grade.VIP);
+        //when
+        int discount = test.discount(member, 10000);
+        //then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("VIP가 아니면 할인 적용이 안되어야 한다.")
+    void vip_x(){
+        //given
+        Member member = new Member(1L,"Basic", Grade.BASIC);
+        //when
+        int discount = test.discount(member, 10000);
+        //then
+
+        assertThat(discount).isEqualTo(1000);
+    }
+
+
+}
